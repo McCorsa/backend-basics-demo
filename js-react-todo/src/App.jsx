e835a6796a293f1d576618737c04337c21c9d0a7
Corey McCormick <coreymccor@live.com.au>
2023-03-19 09:13:38 +1000

    Added some CRUD functions

--- js-react-todo/src/App.jsx
+++ js-react-todo/src/App.jsx
@@ -5,19 +5,37 @@ import Task from './Task'
 function App() {
   const [count, setCount] = useState(0)
   const [newEntry, setNewEntry] = useState("")
-  const [tasks, setTasks] = useState([])
+  const [todos, setTodos] = useState([])
+
+  const addTodo = () => {
+    setTodos([...todos, newEntry])
+  }
+
+  const delTodo = (todo) => {
+    setTodos(todos.filter((x) => x != todo))
+  }
+
+  const editTodo = (oldTitle, newTitle) => {
+    const newTodos = todos.map((x) => {
+      if (x == oldTitle) {
+        return newTitle
+      }
+      return x
+    })
+    setTodos(newTodos)
+  }
 
   return (
     <div className="container">
       <h1>React To-Do App</h1>
       <div className="d-flex gap-3">
         <input type="text" placeholder="New Task" className="flex-fill" value={newEntry} onChange={(e) => setNewEntry(e.target.value)}></input>
-        <button className="btn btn-primary" onClick={() => setTasks([...tasks, newEntry])}>Submit</button>
+        <button className="btn btn-primary" onClick={() => setTodos([...todos, newEntry])}>Submit</button>
       </div>
       <p>New task is {newEntry}</p>
       <h2>Current Tasks</h2>
       <div className="d-flex flex-column gap-1">
-        {tasks.map((task) => <Task title={task}></Task>)}
+        {todos.map((task) => <Task title={task} delTodo={delTodo} editTodo={editTodo}></Task>)}
       </div>
       
     </div>
--- js-react-todo/src/Task.jsx
+++ js-react-todo/src/Task.jsx
@@ -1,20 +1,25 @@
 import { useState } from "react"
 
-function Task({title}) {
+function Task({title, delTodo, editTodo}) {
     const [edit, setEdit] = useState(false)
+    const [editTitle, setEditTitle] = useState(title)
+
+    const completeEdit = () => {
+        editTodo(title, editTitle)
+        setEdit(false)
+    }
 
     return (
         <div className="d-flex flex-fill p-1 gap-2 border align-items-center">
             {edit ? <>
-                <input className="flex-fill" defaultValue={title}></input>
-                <button className="btn btn-success" onClick={() => setEdit(false)}><i className="bi bi-save"></i></button>
-                <button className="btn btn-danger"><i className="bi bi-x-lg"></i></button>
+                <input className="flex-fill" value={editTitle} onChange={(e) => setEditTitle(e.target.value)}></input>
+                <button className="btn btn-success" onClick={() => completeEdit()}><i className="bi bi-save"></i></button>
             </> : <>
                 <div className="flex-fill">{title}</div>
                 <div className="">date</div>
                 <button className="btn btn-primary" onClick={() => setEdit(true)}><i className="bi bi-pen"></i></button>
-                <button className="btn btn-danger"><i className="bi bi-x-lg"></i></button>
             </>}
+            <button className="btn btn-danger" onClick={() => delTodo(title)}><i className="bi bi-x-lg"></i></button>
         </div>
     )
 }
