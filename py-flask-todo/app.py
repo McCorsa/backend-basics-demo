2565018282a7a628d1b4909395a34b7bd17c7f74
Corey McCormick <coreymccor@live.com.au>
2023-04-18 09:27:52 +1000

    A bunch of changes

--- py-flask-todo/app.py
+++ py-flask-todo/app.py
@@ -1,6 +1,7 @@
-from flask import Flask, redirect, render_template, request
+from flask import Flask, redirect, render_template, request, flash
 
 app = Flask(__name__)
+app.secret_key = "supersecretkey"
 
 logged_in = True;
 todos = ["Task 1", "Task 2", "Task 3"]
@@ -19,13 +20,24 @@ def login():
 def add_todo():
     print(request.form["newtask"])
     todos.append(request.form["newtask"])
+    flash("Successfully added new task " + request.form["newtask"])
     return redirect("/")
 
-@app.route("/edit/<int:id>/<new_value>")
-def edit_todo(id, new_value):
-    return f'Editing todo with id {id} to become new_value {new_value}'
+@app.route("/edit/<int:id>/", methods=["GET", "POST"])
+def edit_todo(id):
+    if request.method == "GET":
+        return render_template("edit.html", id=id, todo=todos[id])
+    elif request.method == "POST":
+        todos[id] = request.form["newtask"]
+        flash("Task with id " + str(id) + " has been renamed to " + request.form["newtask"])
+        return redirect("/")
 
-@app.route("/delete/<deltodo>")
-def delete_todo(deltodo):
-    todos.remove(deltodo)
+# @app.route("/edit/<int:id>/<new_value>")
+# def edit_todo(id, new_value):
+#     return f'Editing todo with id {id} to become new_value {new_value}'
+
+@app.route("/delete/<int:id>")
+def delete_todo(id):
+    todos.pop(id)
+    flash("Successfully deleted todo with id " + str(id))
     return redirect("/")
