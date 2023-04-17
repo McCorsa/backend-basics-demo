efa74054ee57bbd94451e1f499deb14ccdff1873
Corey McCormick <coreymccor@live.com.au>
2023-04-17 22:25:00 +1000

    Added more functions to py app

--- py-flask-todo/app.py
+++ py-flask-todo/app.py
@@ -1,28 +1,31 @@
-from flask import Flask, redirect, render_template
+from flask import Flask, redirect, render_template, request
 
 app = Flask(__name__)
 
 logged_in = True;
+todos = ["Task 1", "Task 2", "Task 3"]
 
 @app.route("/")
 def index():
     if not logged_in:
         return redirect("login")
-    todos = ["Task 1", "Task 2", "Task 3"]
     return render_template("home.html", todos=todos)
 
 @app.route("/login")
 def login():
     return render_template("login.html")
 
-@app.route("/add/<todo>")
-def add_todo(todo):
-    return f'Creating new todo {todo}'
+@app.route("/add", methods=["POST"])
+def add_todo():
+    print(request.form["newtask"])
+    todos.append(request.form["newtask"])
+    return redirect("/")
 
 @app.route("/edit/<int:id>/<new_value>")
 def edit_todo(id, new_value):
     return f'Editing todo with id {id} to become new_value {new_value}'
 
-@app.route("/delete/<int:id>")
-def delete_todo(id):
-    return f'Deleting todo with id {id}'
+@app.route("/delete/<deltodo>")
+def delete_todo(deltodo):
+    todos.remove(deltodo)
+    return redirect("/")
